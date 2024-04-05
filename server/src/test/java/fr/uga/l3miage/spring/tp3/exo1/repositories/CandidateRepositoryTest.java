95d2aecc0a387d4960e23f6fd30b6762bdf91b85
l3miage-alptekie <eylul.alptekin@etu.univ-grenoble-alpes.fr>
2024-04-05 15:56:35 +0200

    commit 3

--- server/src/test/java/fr/uga/l3miage/spring/tp3/exo1/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/exo1/repositories/CandidateRepositoryTest.java
@@ -4,12 +4,13 @@ import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
 import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
+import fr.uga.l3miage.spring.tp3.repositories.TestCenterRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith; // Import de ExtendWith
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension; // Import de SpringExtension
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Set;
 
@@ -22,18 +23,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CandidateRepositoryTest {
     @Autowired
     private CandidateRepository candidateRepository;
+    @Autowired
+    private TestCenterRepository testCenterRepository;
 
     @Test
     void testFindAllByTestCenterEntityCode(){
 
         TestCenterEntity testCenterEntity = TestCenterEntity
                 .builder()
-                .code(TestCenterCode.valueOf("GRE"))
+                .code(TestCenterCode.GRE)
                 .build();
 
         TestCenterEntity testCenterEntity2 = TestCenterEntity
                 .builder()
-                .code(TestCenterCode.valueOf("TOU"))
+                .code(TestCenterCode.TOU)
                 .build();
 
         CandidateEntity candidateEntity = CandidateEntity
@@ -50,14 +53,18 @@ public class CandidateRepositoryTest {
                 .testCenterEntity(testCenterEntity2)
                 .build();
 
+        testCenterRepository.save(testCenterEntity);
+        testCenterRepository.save(testCenterEntity2);
         candidateRepository.save(candidateEntity);
         candidateRepository.save(candidateEntity2);
 
-        Set<CandidateEntity> candidateEntitiesResponses = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.valueOf("GRE"));
+        Set<CandidateEntity> candidateEntitiesResponses = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
 
         assertThat(candidateEntitiesResponses).hasSize(1);
 
 
     }
+
+
 
 }
