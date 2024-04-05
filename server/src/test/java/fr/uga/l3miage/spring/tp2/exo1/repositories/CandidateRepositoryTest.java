1668d3fe9e471c5983aef527e43eea8560b1aca6
l3miage-ozyonumb <baran.ozyonum@etu.univ-grenoble-alpes.fr>
2024-04-05 15:23:54 +0200

    test center code

--- server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/repositories/CandidateRepositoryTest.java
@@ -1,5 +1,58 @@
 package fr.uga.l3miage.spring.tp2.exo1.repositories;
 
+import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
+import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
+import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
 public class CandidateRepositoryTest {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Test
+    void testFindAllByTestCenterEntityCode(){
+
+        TestCenterEntity testCenterEntity = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.valueOf("GRE"))
+                .build();
+
+        TestCenterEntity testCenterEntity2 = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.valueOf("TOU"))
+                .build();
+
+
+
+        CandidateEntity candidateEntity = CandidateEntity
+                .builder()
+                .firstname("code Gre")
+                .email("")
+                .testCenterEntity(testCenterEntity)
+                .build();
+
+        CandidateEntity candidateEntity2 = CandidateEntity
+                .builder()
+                .firstname("code Tou")
+                .email("")
+                .testCenterEntity(testCenterEntity2)
+                .build();
+
+        candidateRepository.save(candidateEntity);
+        candidateRepository.save(candidateEntity2);
+
+        Set<CandidateEntity> candidateEntitiesResponses = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.valueOf("GRE"));
+
+        assertThat(candidateEntitiesResponses).hasSize(1);
+
+
+
+
+    }
 
 }
